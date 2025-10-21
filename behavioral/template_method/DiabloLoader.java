385ca443a44f68f4a183911aba37d318191e6cac
Pavan Kalyan Bagam <bagampavankalyan@gmail.com>
2025-10-20 22:08:26 -0700

    behavioral/template-method

---
+++ behavioral/template_method/DiabloLoader.java
@@ -0,0 +1,30 @@
+package behavioral.template_method;
+public class DiabloLoader extends BaseGameLoader {
+
+    @Override
+    public byte[] loadLocalData() {
+        System.out.println("Loading Diablo files...");
+        // Some Diablo Code...
+        return new byte[0];
+    }
+
+    @Override
+    public void createObjects(byte[] data) {
+        System.out.println("Creating Diablo objects...");
+        // Some Diablo Code...
+    }
+
+    @Override
+    public void downloadAdditionalFiles() {
+        System.out.println("Downloading Diablo sounds...");
+        // Some Diablo Code...
+    }
+
+    @Override
+    public void initializeProfiles() {
+        System.out.println("Loading Diablo profiles...");
+        // Some Diablo Code...
+    }
+
+}
+
---
+++ behavioral/template_method/MainApp.java
@@ -0,0 +1,16 @@
+package behavioral.template_method;
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        BaseGameLoader wowLoader = new WorldOfWarcraftLoader();
+        wowLoader.load();
+
+        System.out.println("==========================================");
+
+        BaseGameLoader diabloLoader = new DiabloLoader();
+        diabloLoader.load();
+
+    }
+
+}
